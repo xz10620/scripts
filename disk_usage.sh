ff7c7980a1aa6aab4480a6ce22b665c0f529afe7
Steve Zhao <steve.zhao@sima.ai>
2026-02-17 10:39:07 -0800

    remove vm remote

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,4 +1,4 @@
 # check filesystem usage for your home
-ssh vm 'df -h ~; echo; quota -s || true'
+df -h ~; echo; quota -s || true
 # check how big the vscode server files are
-ssh vm 'du -sh ~/.vscode-server* 2>/dev/null || true'
+du -sh ~/.vscode-server* 2>/dev/null || true
